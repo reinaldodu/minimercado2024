8b70d68720a94fbd3e995e0e7ab307ec5e5022d6
Reinaldo Duque <reinaldodu@gmail.com>
2024-08-13 21:27:31 -0500

    Creación CRUD Productos

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo', 'Nuestros productos')
+@section('cabecera', 'Nuestros productos')
 
 @section('contenido')
     {{-- Botón para crear un producto nuevo --}}
